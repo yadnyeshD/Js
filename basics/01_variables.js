77286d408550fa76e0d3a6782eca504b4832dc4f
yadnyesh Deore <yadnyeshdeore277@gmail.com>
2024-12-13 14:51:18 +0000

    new code

---
+++ basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "Yadnyeshdeore@google.com"
+var accountPassword = "12345"
+accountCity = "Pune"
+
+/*
+Prefer not to use var
+because of bock scope issue
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
---
+++ basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; // Treat all JS code as newer version
+//number
+//bigint
+//string => "az"
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//object
+
+console.log(typeof "Yadnyesh")
+console.log(typeof undefined)
+
+
